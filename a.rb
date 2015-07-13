4301da5c28e844041ff7a4a42ce73fe729d150a0
Jonathan Walton <lemez9@gmail.com>
2015-07-13 01:00:35 +0100

    refactored to make it easier to read

--- a.rb
+++ a.rb
@@ -1,38 +1,46 @@
 require 'marky_markov'
 require 'awesome_print'
 
-markov = MarkyMarkov::Dictionary.new('dictionary2', 2) # Saves/opens dictionary.mmd
-# source = "/Users/JW/Dropbox/_w/T10 NLP/corpus files/original corpus files"
-# problematic_files = ['Il_Divo/Passera','Il_Divo/Ti_Amero', 'Lady_Gaga/Scheisse', 'Feliz_Navidad']
+@markov = MarkyMarkov::Dictionary.new('dictionary2', 2) # Saves/opens dictionary.mmd
 
-# Dir.glob("#{source}/*/*/*.txt").each do |file|
 
-# 	begin
-# 		problematic_files.each{|a| next if file.include?(a)}
-# 		markov.parse_file file
-# 	rescue ArgumentError => e
-# 		puts "#{e}: #{file}"
-# 	end
-	 	
-# end
-# markov.save_dictionary!
+def make_dictionary
+	source = "/Users/JW/Dropbox/_w/T10 NLP/corpus files/original corpus files"
+	problematic_files = ['Il_Divo/Passera','Il_Divo/Ti_Amero', 'Lady_Gaga/Scheisse', 'Feliz_Navidad']
 
-# 5.times do
-# 		num = Random.rand(11) + 10
-# 		words = markov.generate_n_words num
-# 		words.split(" ").each do |w|
-# 			words.delete!(w) if w.downcase.include?("chorus") or w.downcase.include?("verse") \
-# 							or w.downcase.include?("[") or w.downcase.include?("\\") 
-# 		end
-		
-# 		p "#{num}:#{words}"		
-# end
+	Dir.glob("#{source}/*/*/*.txt").each do |file|
 
-sentences = markov.generate_n_sentences 5
-lines = sentences.split(" ").map! {|s| s[0]==s[0].upcase && s!="I" ? s="\n#{s}" : s }.join(" ") 
-lines.split("\n").map(&:strip).reject {|a| !a.include?(" ")}.each do |o| 
-	syll = eval "->s{s.scan(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/).size}" #get syllable size
-	p "#{syll.call(o)}:#{o}"
+		begin
+			problematic_files.each{|a| next if file.include?(a)}
+			@markov.parse_file file
+		rescue ArgumentError => e
+			puts "#{e}: #{file}"
+		end
+		 	
+	end
+	@markov.save_dictionary!
+end
+
+def print_words
+	5.times do
+			num = Random.rand(11) + 10
+			words = @markov.generate_n_words num
+			words.split(" ").each do |w|
+				words.delete!(w) if w.downcase.include?("chorus") or w.downcase.include?("verse") \
+								or w.downcase.include?("[") or w.downcase.include?("\\") 
+			end
+			
+			p "#{num}:#{words}"		
+	end
+end
+
+def print_sentences num
+	sentences = @markov.generate_n_sentences num
+	lines = sentences.split(" ").map! {|s| s[0]==s[0].upcase && s!="I" ? s="\n#{s}" : s }.join(" ") 
+	lines.split("\n").map(&:strip).reject {|a| !a.include?(" ")}.each do |o| 
+		syll = eval "->s{s.scan(/[aiouy]+e*|e(?!d$|ly).|[td]ed|le$/).size}" #get syllable size
+		p "#{syll.call(o)}:#{o}"
+	end
 end
 
 
@@ -40,40 +48,45 @@ end
 
 ##### Separate dictionaries for each artist - not working very well as not enough input sources currently
 
-# Dir.glob("#{source}/*/*").each do |folder|
+def make_artist_dictionaries
+	Dir.glob("#{source}/*/*").each do |folder|
 
-# 	path,artistname = File.split(folder)
-# 	p artistname
+		path,artistname = File.split(folder)
+		p artistname
 
-# 	markov = MarkyMarkov::Dictionary.new("#{artistname}") # Saves/opens dictionary.mmd
-# 		Dir.glob("#{folder}/*.txt").each do |f|
-# 			begin
-# 				problematic_files.each{|a| next if f.include?(a)}
-# 				markov.parse_file f
-# 			rescue ArgumentError => e
-# 				puts "#{e}: #{f}"
-# 			end
-# 		end
-# 	markov.save_dictionary!
-# end
+		markov = MarkyMarkov::Dictionary.new("#{artistname}") # Saves/opens dictionary.mmd
+			Dir.glob("#{folder}/*.txt").each do |f|
+				begin
+					problematic_files.each{|a| next if f.include?(a)}
+					markov.parse_file f
+				rescue ArgumentError => e
+					puts "#{e}: #{f}"
+				end
+			end
+		markov.save_dictionary!
+	end
+end
 
-# Dir.glob("#{Dir.pwd}/*.mmd").each do |dictionary|
-# 	artist = File.basename(dictionary, ".mmd")
-# 	markov = MarkyMarkov::Dictionary.new(artist)
-# 	p "+++#{artist}++++"
-# 	5.times do
-# 		num = Random.rand(11) + 10
-# 		words = markov.generate_n_words num
-# 		words.split(" ").each do |w|
-# 			words.delete!(w) if w.downcase.include?("chorus") or w.downcase.include?("verse") \
-# 							or w.downcase.include?("[") or w.downcase.include?("\\") 
-# 		end
-		
-# 		p "#{num}:#{words}"
-		
-# 	end
-# 	p "---"
-# end
+def print_examples_from_artist_dictionaries
+	Dir.glob("#{Dir.pwd}/*.mmd").each do |dictionary|
+		artist = File.basename(dictionary, ".mmd")
+		markov = MarkyMarkov::Dictionary.new(artist)
+		p "+++#{artist}++++"
+
+		5.times do
+			num = Random.rand(11) + 10
+			words = markov.generate_n_words num
+			words.split(" ").each do |w|
+				words.delete!(w) if w.downcase.include?("chorus") or w.downcase.include?("verse") \
+								or w.downcase.include?("[") or w.downcase.include?("\\") 
+			end
+			
+			p "#{num}:#{words}"
+			
+		end
+		p "---"
+	end
+end
 
 
 
---
+++ code.rb
@@ -0,0 +1,5 @@
+require_relative "./a"
+
+#usage ruby code.rb
+
+print_sentences 3
